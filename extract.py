99cbc9d06c55a845073e093844b964015cd50949
SAIF AHMED <SAHMED@KINETICA.COM>
2021-10-21 11:38:32 -0400

    Includes time series extractor

--- extract.py
+++ extract.py
@@ -1,123 +1,55 @@
 import os
 import sys
-import copy
-import time, datetime
-import math
-from functools import reduce
-from operator import add
-import threading
+from datetime import datetime
+import csv
 
-from iex_parser import Parser, TOPS_1_6
-from pprint import pprint
+from IEXTools import Parser, messages
+
+
+# securities of interest (limit this, or your outputs will be huge!)
+SOI = ['AIG', "ACN", "GS", "OMC"]
+SOI = ["GME", "AMC", "CLOV", "AFRM", "WISH"]
+
 
 # get data from https://iextrading.com/trading/market-data/
-
-#DATA_FILE = 'data_feeds_20210303_20210303_IEXTP1_TOPS1.6.pcap.gz'
-#DATA_FILE = 'data_feeds_20210303_20210303_IEXTP1_TOPS1.6.pcap'
-DATA_FILE = 'data_feeds_20191219_20191219_IEXTP1_TOPS1.6.pcap.gz'
-
-#SOI = ['QQQ', 'SPY', 'TSLA', 'GME', 'PLTR', 'GBTC', 'MARA']
-SOI = ['QQQ', 'SPY']
-EMAS_TO_CALC = [3, 5, 11]
-
-latest = {}
-snaps = []
-emas = {}
-tapetime = None
-
-for e in EMAS_TO_CALC:
-	emas[e] = []
+DATA_FOLDER = "/Users/saif/Downloads"
+DATA_FILE = 'data_feeds_20211019_20211019_IEXTP1_TOPS1.6.pcap'
+DATA_FILE = 'data_feeds_20211018_20211018_IEXTP1_TOPS1.6.pcap'
+DATA_FILE = 'data_feeds_20211015_20211015_IEXTP1_TOPS1.6.pcap'
+OUTFILE_CSV = "ticker_tape.csv"
 
 
-def show_ts():
-	toi = "SPY"
-	for i, s in reversed(list(enumerate(snaps))):
-		if toi in s:
-			(ts, b, a) = s[toi]
-			ema5 = ""
-			ema3 = ""
-			print(emas)
-			if len(emas[5]>= i):
-				if toi in emas[5][i]:
-					if len(emas[3])>= i:
-						ema3 = emas[3][-i][toi]
-					if len(emas[5])>= i:
-						ema5 = emas[5][-i][toi]
+HEADER = ['timestamp', 'symbol', 'bid_size', 'bid_price', 'ask_price', 'ask_size']
 
-			print (f"{ts} \tb: {b:4.2f}  a:{a:4.2f}  m:{round((b+a)/2,2):4.2f} ema3: {ema3}  ema5: {ema5}")
+def save_quotes(input_pcap, output_csv, secs_of_interest):
 
+	csv_file = open(output_csv, "w")
+	writer = csv.writer(csv_file, delimiter=',')
+	writer.writerow(HEADER)		
 
-def calc_ema():
-	global emas
+	allowed = [messages.QuoteUpdate]
+	with Parser(input_pcap, tops=True, deep=False) as iex_messages:
+		while True:
+			x = iex_messages.get_next_message(allowed)
+			if x.symbol in SOI:
+				if x.bid_size!=0 and x.ask_size!=0:
+					dt_object = datetime.fromtimestamp(int(str(x.timestamp)[:10]))
 
-	for e in EMAS_TO_CALC:
-		if len(snaps) < e:
-			print(f"Too little historical snapshots to calculate {e} period EMA")
-		else:
-			print(f"Calculating EMA{e}")
-			records_of_interest = snaps[:e]
-			ema_insertable = {}
-			for s in SOI:
-				ema_components = []
-				for r in records_of_interest:
-					if s in r:
-						(t, b, a) = r[s]
-						m = round((b + a) / 2, 2)
-						if m != 0 :
-							ema_components.append(m)
-				if (len(ema_components)) > 0:
-					ema = round(reduce(add, ema_components) / len(ema_components), 2)
-					print(f"EMA Components {ema_components}   ---> {ema}")
-					ema_insertable[s] = ema
-			emas[e].append(ema_insertable)
-
-
-def clocker():
-
-	global latest
-	global snaps
-	global tapetime
-
-	min_current = None
-	min_last = None
-
-	while True:
-		if tapetime:
-			min_current = tapetime.timetuple().tm_min
-
-			if min_last != min_current:
-				# we just turned to a new minute
-				print(f"First quote for the minute: {tapetime}  hist avail: {len(snaps)}")
-				snaps.append(copy.deepcopy(latest))
-				calc_ema()
-				show_ts()
-		else:
-			time.sleep(.5)
-	
-		min_last = min_current
-		#time.sleep(.5)
-	
-def save_quotes():
-	global tapetime
-	with Parser(DATA_FILE, TOPS_1_6) as reader:
-		for message in reader:
-			if message['type']=='quote_update':
-				s = message['symbol'].decode()
-				if s in SOI:
-					b = float(message['bid_price'])
-					a = float(message['ask_price'])
-					t = message['timestamp']
-					tapetime = t
-					latest[s]=(t, b, a)					
+					tick = {
+						"timestamp": x.timestamp,
+						"symbol": x.symbol,
+						"bid_size": x.bid_size,
+						"bid_price": x.bid_price_int/10000,
+						"ask_price": x.ask_price_int/10000,
+						"ask_size": x.ask_size
+						}
+					print(dt_object, tick)
+					writer.writerow([x.timestamp,x.symbol,x.bid_size,tick['bid_price'],tick['ask_price'],x.ask_size])
 
 if __name__ == "__main__":
 
-	print("Starting thread monitor clock", end="")
-	threading.Thread(target=clocker).start()
-	print("...started")
-
-	print("Starting thread to read quotes off the wire", end="")
-	threading.Thread(target=save_quotes).start()
-	print("...started")
-
+	save_quotes(input_pcap=f"{DATA_FOLDER}/{DATA_FILE}", 
+				output_csv=OUTFILE_CSV, 
+				secs_of_interest=SOI)
+	
 
